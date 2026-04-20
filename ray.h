296e77a01d563a8badf04eec0526dde29eb4ab98
Preksha <prekshachaki13@gmail.com>
2026-04-20 12:43:03 +0000

    implemented ray.h in main.cpp

--- ray.h
+++ ray.h
@@ -1,24 +1,24 @@
-#ifdef RAY_H
+#ifndef RAY_H
 #define RAY_H
-
 #include "vec3.h"
 
 class ray{
 public:
 ray(){}
 //constructor and initialization
-ray(const point3& origin, const vec3& direction : orig(origin),dir(direction)){}
+ray(const point3& origin, const vec3& direction): orig(origin),dir(direction){}
 
+//getters
 const point3& origin() const {return orig;}
 const vec3& direction() const {return dir;}
 
 point3 at(double t) const{
-return orig+t*dir
+return orig+t*dir;
 }
 
 private:
-point3& orig;
-vec3& dir;
+point3 orig;
+vec3 dir;
 };
 
 #endif
